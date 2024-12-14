42333ed76653cfe7fe362db71108cb5ffae66867
Violet Giraffe <VioletGiraffe@users.noreply.github.com>
2024-12-14 16:10:12 +0200

    Output dir fixed, Github workflow added

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
+#include "assert/advanced_assert.h"
 
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
+	// Testing that the library is in fact linked correctly by calling a function from it
+	AdvancedAssert::setLoggingFunc([](const char* msg) {
+		std::cout << msg << '\n';
+	});
+
 	return 0;
 }
